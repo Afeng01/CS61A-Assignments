f1a21c8c6603dd2a384aa369227a523e01c14d5c
Afeng01 <53217394+Afeng01@users.noreply.github.com>
2025-08-14 10:14:32 +0800

    完成了1.6高阶函数之前的东西，基本重新做了一遍。

--- HW/hw01/hw01.py
+++ HW/hw01/hw01.py
@@ -13,10 +13,11 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = sub
     else:
-        f = _____
+        f = add
     return f(a, b)
+    #正常是 a + b 这种，但当 f 是在操作符的位置，那么就需要使用 add 这种
 
 def a_plus_abs_b_syntax_check():
     """Check that you didn't change the return statement of a_plus_abs_b.
@@ -32,7 +33,6 @@ def a_plus_abs_b_syntax_check():
 def two_of_three(i, j, k):
     """Return m*m + n*n, where m and n are the two smallest members of the
     positive numbers i, j, and k.
-
     >>> two_of_three(1, 2, 3)
     5
     >>> two_of_three(5, 3, 1)
@@ -42,7 +42,16 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return (i + j + k - min(i,j,k) - max(i,j,k))**2 + (min(i,j,k))**2
+#   用三个数的和，减去最大的，减去最小的，得到中间的，然后平方，再加上最小的。2025年8月13日
+#   自己想出来的，然后发现，其实整体的表达式是可以省略一些的。看了看下面的内容：
+#    就是用a的平方+b的平方+c的平方，减去最大数的平方，好简洁啊
+#    i**2 + j**2 + k**2 - max(i,j,k)**2
+#   i**2 代表的意思是: i的平方 ; 如果写成i**i , 就是在说i 的 i 次方了
+#   除非是 i * i , 那可能意思才是 i 的平方吧?
+#   i**i + j**j + k**k - max(i,j,k)*max(i,j,k)
+#   min(i*i+j*j, i*i+k*k, j*j+k*k)
+#   add(min(i,j,k)*min(i,j,k))
 
 def two_of_three_syntax_check():
     """Check that your two_of_three code consists of nothing but a return statement.
@@ -54,7 +63,6 @@ def two_of_three_syntax_check():
     """
     # You don't need to edit this function. It's just here to check your work.
 
-
 def largest_factor(n):
     """Return the largest factor of n that is smaller than n.
 
@@ -65,8 +73,33 @@ def largest_factor(n):
     >>> largest_factor(13) # factor is 1 since 13 is prime
     1
     """
+    result = n - 1
+    while( result > 0 ):
+        if (n % result == 0 ):
+            return result
+        else:
+            result = result-1 #经测试,if-else后面带不带return似乎都可以
     "*** YOUR CODE HERE ***"
-
+    """大概是2025年8月13日-6month前修改。
+    factor = n - 1
+    while(factor > 0):
+        if(n % factor == 0):#这个时候就已经是正确答案了,就可以直接返回
+            return factor
+        factor -= 1
+2025年8月13日的 code 
+# factor = 1;
+# while(factor <= n):
+#     if n > 1:
+#         n % factor == 0
+#         print(factor)
+#         factor = factor + 1
+#         return max()
+#     else:
+#         return
+#   2025年8月13日，你习惯性地从最小的开始遍历，但其实可以从最大的(也就是比 n 小一个的)地方开始
+    **返回最大的，那就从最大的开始**
+    **而且这还是个很常见的循环遍历系统**
+"""
 
 def hailstone(n):
     """Print the hailstone sequence starting at n and return its
@@ -88,4 +121,63 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
-
+    '''我先实现的if-else,然后发现if只能判断一次,
+    但我需要判断多次,所以需要在外面套上一个while,来不断循环'''
+    """steps = 0 
+    result = n  #这个步骤是多余的
+    while(result >= 1):
+        print(result)
+        steps += 1
+        if(result % 2 == 0): #如果是偶数;如果是奇数;如果是1
+            result = result//2
+            print(result)   
+            # 然后你会发现,这个print和循环开始时候的print是重复的;
+            # 如果这里打印了,再进入循环的时候,还会打印一次
+        else:
+            result = result * 3 + 1
+            print(result)"""
+    """最后这里我不清楚,如何把步骤和步骤数都打印出来
+    好简洁阿,参考答案的步骤;另外,不需要在if那里写return吗?"""
+    steps = 1 
+    while(n != 1):
+        print(n)
+        # steps += 1 # 这个正常是放在else下面的
+        if(n % 2 == 0): #如果是偶数;如果是奇数;如果是1
+         # if后面可以不用带括号,带括号似乎是Java留下来的习惯
+            n = n//2
+        else:
+            n = n * 3 + 1
+        steps += 1 
+        # 放在最后是因为,初始步骤为1,
+        # 如果在外面设置为 0 的话,1的话不进入循环,
+        # 然后步骤就是 0 了
+    # while循环中都是不等于1的情况,
+    # 如果最后n=1了,哪还需要再打印一次
+    print(n)
+    return steps # 我不明白这一点 , 放在这里 , 有何作用(b和b=)
+# 编写一个函数，该函数接收参数 n，打印出从 n 开始的冰雹序列，并返回序列的步数
+    """
+    foots = 0;//这里在想要不要默认 1 ，可能默认 1 会更好
+    while(n != 0): 
+    # n 得常在，重新赋值到 n ，我是直接返回的值，但忽略了返回之后 n 就只能出现一次了
+        if(n == 1):
+            print(1)
+            foots = foots + 1
+        else:
+            print(n)
+            if(n % 2 == 0):
+                foots = foots + 1 
+                //这里可以看出，每一个步骤都有 foots，于是可以把他们提出来，放在循环的外面
+                return n // 2 
+                //这里不要直接返回，要重新赋值给 n ，这样才能在 while 循环的时候一直进行下去
+            else:
+                foots = foots + 1
+                //这个可以简写成 foots += 1
+                //看一下上面的 print(n)，因为赋值了 n 之后，还满足条件的时候，是会进入 while 的
+                //在 while 循环没结束的时候，是会进入到循环体内 print 出来的
+                //但如果循环体结束了，就会有最后一次的没有打印出来，所以那里还需要放上一个
+                return (n * 3) + 1
+    return foots # 返回总步数
+    //return 的时候，就退出这个程序了
+    写个作业，40分钟……然后复盘要20分钟
+    """
